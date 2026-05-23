4d7f5842e562b717b9b208416c63ef41ba25d510
bruce-deekay <88024202+bruce-deekay@users.noreply.github.com>
2026-05-23 20:02:36 +0300

    basic add/subtract app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
+function App(){
+  const [count, setCount] = useState(0);
+
+  function handleAdd(){
+    setCount(count+1);
+    console.log(count);
+  }
+
+  function handleSub(){
+    setCount(count-1);
+    console.log(count);
+  }
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Box'>
+        <p>{count}</p>
+        <button onClick={handleAdd} className='add'>Add</button>
+        <button onClick={handleSub} className='sub'>Sub</button>
+      </div>
+      <h1>Hello WOrld!</h1>
     </div>
   );
 }
